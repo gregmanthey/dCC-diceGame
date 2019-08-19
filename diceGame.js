864d807265d0a030543636980b5d19a77ca60966
gregmanthey <gmanthey03@gmail.com>
2019-08-19 15:23:37 -0500

    Added pitch menu, skeleton functions for batting and game summary

--- diceGame.js
+++ diceGame.js
@@ -1,15 +1,15 @@
 function runDiceGame() {
   runSplashScreen();
   let home = isHome();
-  // if (home) {
-  //   startPitching();
-  //   startBatting();
-  // }
-  // else {
-  //   startBatting();
-  //   startPitching();
-  // }
-  // runGameSummary();
+  if (home) {
+    startPitching();
+    startBatting();
+  }
+  else {
+    startBatting();
+    startPitching();
+  }
+  runGameSummary();
 }
 
 function rollDice(numOfSides) {
@@ -22,25 +22,56 @@ function runSplashScreen () {
 }
 
 function isHome() {
-  let home;
-  while(home == undefined) {
+  while(true) {
     switch (prompt("Would you like to be home (1) or away (2) ?")) {
-      case "home":
-        case "Home":
-          case "1":
-            home = true;
-            return home;
-            break;
-      case "away":
-        case "Away":
-          case "2":
-            home = false;
-            return home;
-            break;  
+      case "1":
+        return true;
+        break;
+      case "2":
+        return false;
+        break;  
       default:
+        console.log("Please enter a valid choice: 1 or 2.")
         break;
     }
   }
+}
+
+function startBatting () {
+
+}
+
+function startPitching () {
+  //select a pitch
+  let outs = 0;
+  while (outs < 3) {
+    let pitch = choosePitch();
+    outs = 3; //exit loop for testing
+  }
+  //roll different die based on pitch selection
+}
+
+function choosePitch () {
+  let pitch;
+  while (pitch == undefined) {
+    switch (prompt("Choose a pitch: 1) Fastball 2) Changeup 3) Curveball")) {
+      case "1":
+        return "Fastball";
+        break;
+      case "2":
+        return "Changeup";
+        break;
+      case "3":
+        return "Curveball";
+        break;
+      default:
+        console.log("Please enter a valid choice: 1, 2, or 3.")
+    }
+  }
+}
+
+function runGameSummary () {
+  
 }
 
 let fastball = {
