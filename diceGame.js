6dead8d2b4d200bbbc3a48ba2cc59b5b99943802
gregmanthey <gmanthey03@gmail.com>
2019-08-19 14:56:13 -0500

    Added basic game logic, home/away prompt, initial fastball object

--- diceGame.js
+++ diceGame.js
@@ -1,5 +1,15 @@
 function runDiceGame() {
   runSplashScreen();
+  let home = isHome();
+  // if (home) {
+  //   startPitching();
+  //   startBatting();
+  // }
+  // else {
+  //   startBatting();
+  //   startPitching();
+  // }
+  // runGameSummary();
 }
 
 function rollDice(numOfSides) {
@@ -9,4 +19,32 @@ function rollDice(numOfSides) {
 function runSplashScreen () {
   console.log("Welcome to the greatest baseball dice game of all time!")
   console.log("Now let us begin.")
+}
+
+function isHome() {
+  let home;
+  while(home == undefined) {
+    switch (prompt("Would you like to be home (1) or away (2) ?")) {
+      case "home":
+        case "Home":
+          case "1":
+            home = true;
+            return home;
+            break;
+      case "away":
+        case "Away":
+          case "2":
+            home = false;
+            return home;
+            break;  
+      default:
+        break;
+    }
+  }
+}
+
+let fastball = {
+  hitChance: 90,
+  hitPower: 90,
+  accuracy: 90,
 }
